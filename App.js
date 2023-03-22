d20362e16be61385eec652f9573f1d2c826b5ce5
samuelberhane <1samibrhane@gmail.com>
2023-03-22 19:10:32 +0300

    Update routes.

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // const updateNews = async () => {
+  //   const { data } = await axios.patch(
+  //     "http://10.0.2.2:8000/api/news/641959738ef4f856316ac446",
+  //     { meta: "covid,vaccine,medicine" }
+  //   );
+
+  //   console.log("response: " + JSON.parse(data));
+  // };
+
+  // updateNews();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
